b06a704b695e19f1e5c1e61d323219599a86eb7a
Vika <vg.dev31@gmail.com>
2024-10-19 21:49:32 +0400

    Add custom cell, "chat" table with mock-messages

--- Flash Chat iOS13/Controllers/ChatViewController.swift
+++ Flash Chat iOS13/Controllers/ChatViewController.swift
@@ -14,11 +14,20 @@ class ChatViewController: UIViewController {
     @IBOutlet weak var tableView: UITableView!
     @IBOutlet weak var messageTextfield: UITextField!
     
+    var messages: [Message] = [
+        Message(sender: "1@23.com", body: "Hey!"),
+        Message(sender: "a@b.com", body: "Hello!"),
+        Message(sender: "1@23.com", body: "What's up?")
+        ]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        tableView.dataSource = self
         title = K.appName
         navigationItem.hidesBackButton = true
+        
+        tableView.register(UINib(nibName: K.cellNibName, bundle: nil), forCellReuseIdentifier: K.cellIdentifier)
         
     }
     
@@ -35,3 +44,16 @@ class ChatViewController: UIViewController {
         }
     }
 }
+
+extension ChatViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return messages.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: K.cellIdentifier, for: indexPath) as! MessageCell
+        cell.textLabel?.text = messages[indexPath.row].body
+        return cell
+    }
+}
+
---
+++ Flash Chat iOS13/Message.swift
@@ -0,0 +1,14 @@
+//
+//  Message.swift
+//  Flash Chat iOS13
+//
+//  Created by Vika on 19.10.24.
+//  Copyright © 2024 Angela Yu. All rights reserved.
+//
+
+import Foundation
+
+struct Message {
+    let sender: String //1@2.com
+    let body: String
+}
